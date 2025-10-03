5153efa4919736e511ffbfe8225d8e648c32be82
Bhavani <bhavaniviswanathan97@gmail.com>
2025-10-03 16:22:40 +0530

    added more projects

---
+++ javascript/operators.js
@@ -0,0 +1,347 @@
+/*var a=10
+var b=++a
+console.log(a)
+
+
+var a=10
+var b=a++
+console.log(a)*/
+
+//funtion
+
+function parkjimin()
+{
+   console.log("sarengee jimin") 
+}
+
+function  jeonjunkook()
+{
+console.log("sarengee jeonjunkook")
+}
+
+function kimteayung()
+{
+console.log("sarengee kim teayung")
+}
+
+function jeonhoeseok()
+{
+console.log("sarengee jeon hoeseok")
+}
+
+function minyongii()
+{
+ console.log("sarengee min yongii")   
+}
+
+jeonhoeseok()
+
+
+// added operators
+/*
+var a=10;
+var b=10;
+ 
+ function added()
+ {
+    console.log(a+b)
+ }
+ added()*/
+
+ /* var  favactor= "karthik"
+ var  favplayer= "ronaldo"
+ var  favmovies= "thoozha"
+
+ function favourate()
+ {
+    console.log("favourate actor:"+ favactor)
+    console.log("favourate player:"+ favplayer)
+    console.log("favourate movies:"+favmovies)  
+ } 
+favourate()*/
+
+/*
+function area(length,bredth)
+{
+console.log(lenghth+breadth)
+}
+area()*/
+
+/* function area(l,b)
+{
+var a=(l*b)
+console.log("area is:"+a)
+}
+var length=10
+var breadth=20
+area(10,20)*/
+
+
+ 
+ /* function tiktok ()
+ {
+   console.log( "its already baned")
+ }
+function instragram ()
+{
+   console.log( "is peeking on socal mediea")
+}
+function facebook ()
+{
+   console.log("its average to using on social medias")
+}
+tiktok ()*/
+
+/* function samsung()
+{
+   console.log("this is a samsung")
+}
+function oppo ()
+{
+   console.log( "this is a oppo")
+}
+function redmi()
+{
+   console.log("this is a redmi")
+}
+samsung()
+oppo()
+redmi()*/
+
+/* var a=20
+var b=30
+function add()
+{
+
+   console.log(a+b)
+}
+add()*/
+
+// functions
+/* var fvactor="Dhanush"
+var fvmovies= "asuran"
+var fvsongs= "sathiyama nee enakudheevayaeh illa"
+function favourable ()
+{
+   console.log("favourate actor:"+fvactor)
+   console.log("favourate movies:"+fvmovies)
+  console.log("favourate songs"+fvsongs  )
+     
+   
+}
+favourable()*/
+
+ /* function area(length,breadth)
+{
+   console.log(length+breadth)
+}
+area(20,20)*/
+
+
+//parameters
+ /* function area (coffe,tea)
+ {
+
+  var coffe=25;
+  var tea=15;
+   
+   var a =coffe*tea
+   console.log(a)
+ }
+
+ area()*/
+
+ /*function add(a,b)
+ {
+  
+   var a=a+b;
+   console.log( a)
+ }
+ add(20,20)*/
+
+ // return type
+ function myname()
+ {
+
+
+   return("john")
+  }
+ 
+ /* function add(a,b)
+  {
+   return(a+b)
+  }
+  var totally=add(30,30)
+  console.log(totally)
+
+  function fruits(aplle,orange,mangoes,dragonfruits)
+  {
+   return(aplle+orange+mangoes+dragonfruits)
+   
+  }
+  var fruittotally=fruits(30,40,25,50)
+  console.log(fruittotally)*/
+
+  //if else
+   /*if(false)
+   {
+      console.log("take an umberlla")
+   }
+   else{
+      console.log("enjoy the sunshine")
+   }
+   if(false)
+   {
+      console.log("geat job")
+   }
+   else
+   {
+      console.log("before complete the homework after go for playing")
+   }*/
+/*  excersice in if else
+   var rain=false
+   if(rain)
+   {
+      console.log("take an umberlla")
+   }
+   else{
+      console.log("enjoy the sunshine")
+   }
+
+   var finish=true
+   if(finish)
+   {
+      console.log("great job")
+   }
+   else{
+      console.log("finish your homework after playing")
+   }
+
+   var cokkies=true
+   if(cokkies)
+   {
+      console.log("would you like cokkies")
+   }
+   else{
+      console.log("try to bake more cokkie")
+   }
+    var brush=true
+    if(brush)
+    {
+      console.log("your teath are clean")
+    }
+    else
+    {
+     console.log("first you goto brush your teath now") 
+    }
+
+    var yourbirthday=true
+    if(yourbirthday)
+    {
+      console.log("happy birthday enjoy your special day")
+    }
+    else{
+      console.log("have a great day")
+    }*/
+
+      
+  var  color ="red"
+  if(color=="red")
+  {
+   console.log("start")
+  }
+  if(color=="yellow")
+  {
+   console.log("get ready")
+  }
+  if(color=="green")
+  {
+   console.log("stop")
+  }
+
+
+  var sessons="winter"
+  if(sessons=="spring")
+  {
+   console.log("enjoy the blooming flowers")
+  }
+  if(sessons=="summer")
+  {
+   console.log("hava a fun in the sun")
+  }
+  if(sessons=="audumn")
+  {
+   console.log("admire the colourful leaves")
+  }
+  if(sessons=="winter")
+{
+   console.log("Bundle up and stay warm")
+}
+/*var score=80
+  elseif(score=<50)
+{
+   console.log("you need to improve")
+}
+elseif (score= <50 && score=> 70)
+{
+   console.log("good job")
+}
+ elseif(score= <70)
+{
+   console.log("Excellent performence")
+}*/
+
+var number="even"
+if(number=="even")
+{
+   console.log("the number is even")
+}
+if(number=="add")
+{
+   console.log("the number is add")
+}
+
+var characters="a,e,i,o,u"
+if(characters=="a,e,i,o,u")
+{
+   console.log("its is a vowel")
+}
+if(characters=="constant")
+{
+   console.log("its a constant")
+}
+
+var score=80
+   if(score<=50)
+{
+   console.log("you need to improve")
+}
+else if (score >=50 && score<=70)
+{
+   console.log("good job")
+}
+ else if(score>=70)
+{
+   console.log("Excellent performence")
+}
+
+//for loop
+for(count=1;count<=5;count=count+1)
+{
+   console.log("john")
+}
+
+/* for(count=1;count<=10;count=count+1)
+{
+   console.log(count)
+}*/
+
+/* for(count=1;count<=10;count=count+2)
+{
+   console.log(count)
+}*/
+
+for(count=10;count<=1;count=count+1)
+{
+   console.log(count)
+}
